400a35c6b6f3ec5ce038e77bda1f0d5156cfffc9
TmTower <56515784+TmTower@users.noreply.github.com>
2020-05-24 18:43:07 +0300

    Add files via upload

---
+++ bullet.h
@@ -0,0 +1,17 @@
+#ifndef BULLET_H
+#define BULLET_H
+
+#include "head_libs.h"
+
+class Bullet:public QObject,
+        public QGraphicsPixmapItem
+{
+    Q_OBJECT
+public:
+    Bullet(QGraphicsItem*parent=0);
+
+public slots:
+    void move();
+};
+
+#endif // BULLET_H
---
+++ head_libs.h
@@ -0,0 +1,27 @@
+#ifndef HEAD_LIBS_H
+#define HEAD_LIBS_H
+
+#include <QGraphicsRectItem>
+#include <QGraphicsTextItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsItem>
+#include <QObject>
+#include <QWidget>
+#include <QTimer>
+#include <QList>
+#include <stdlib.h>
+#include <QDebug>
+#include <QFont>
+#include <QGraphicsPixmapItem>
+#include <QBrush>
+#include <QImage>
+#include <QtCore>
+#include <QtGui>
+#include <QGraphicsSceneMouseEvent>
+
+#include "algorithm"
+
+using namespace std;
+
+#endif // HEAD_LIBS_H
